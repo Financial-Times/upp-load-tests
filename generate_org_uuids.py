1a7580696ddd8ed282bcf716b7110849f3dc9002
Carlos Roman <carlos.roman@ft.com>
2016-01-15 15:03:48 +0000

    Added most annotated file to make sure they're used in load tests.

--- generate_org_uuids.py
+++ generate_org_uuids.py
@@ -20,10 +20,11 @@ console.setFormatter(formatter)
 
 logging.info('Generating UUIDS for organisation.')
 
-organisations_response = requests.get('http://ftaps39395-law1b-eu-t/transformers/organisations', stream=True)
+organisations_response = requests.get('http://ftaps39403-law1a-eu-t/transformers/organisations', stream=True)
 # uuids_json = cjson.decode(organisations_response.text)
 # api_url_jpath = parse('.[].apiUrl')
 
+most_annotated = open('src/test/resources/organisation/most_annotated.uuid', 'r')
 result_file = open('src/test/resources/organisation/organisations.uuid', 'w')
 result_file.write('%s\n' % 'uuid')
 # for url in api_url_jpath.find(uuids_json):
@@ -31,6 +32,16 @@ for obj in json.load(StringIO(organisations_response.content)):
     if random.randint(0, 15) == 0:
         uuid_re = re.search('[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', obj['apiUrl'])
         result_file.write('%s\n' % uuid_re.group(0))
+        if random.randint(0, 12) == 0:
+            most_uuid = most_annotated.readline()
+            if most_uuid != '':
+                result_file.write('%s' % most_uuid)
+
+most_uuid = most_annotated.readline()
+
+while most_uuid != '':
+    result_file.write('%s' % most_uuid)
+    most_uuid = most_annotated.readline()
 
 result_file.close()
 logging.info('Done')
