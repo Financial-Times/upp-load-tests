9a58642162b0ef22dc134049e983c4a3b52cdb4c
Sandor Torok <sandor.torok@ft.com>
2016-09-06 16:34:04 +0300

    Notifications now are get 3 months before current date with random timestamp

--- src/test/scala/notifications/Simulations.scala
+++ src/test/scala/notifications/Simulations.scala
@@ -1,12 +1,16 @@
 package notifications
 
+import com.github.nscala_time.time.Imports._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import org.slf4j.LoggerFactory
 import utils.LoadTestDefaults._
 
 import scala.language.postfixOps
+import scala.util.Random
 
 object ReadSimulation {
+  private val Logger = LoggerFactory.getLogger(getClass)
 
   val Duration = Integer.getInteger("soak-duration-minutes", DefaultSoakDurationInMinutes)
 
@@ -15,31 +19,32 @@ object ReadSimulation {
     .basicAuth(System.getProperty("username", "username"), System.getProperty("password", "password"))
     .userAgentHeader("EnrichedContent/Load-test")
 
+  val Feeder = Iterator.continually(Map("since" -> DateTime.now().minusMonths(3).plusMillis(Random.nextInt(1000)).toString(DateTimeFormat.forPattern("YYYY-MM-dd'T'HH:mm:ss.SSS'Z'"))))
   val Scenario = scenario("Notifications Read").during(Duration minutes) {
-    exec(http("Notifications Read request")
-      //since should be randomly generated going back 3 month
-      .get("/content/notifications?since=2016-08-13T08:18:40.630Z")
-      .check(status is 200))
+    feed(Feeder).
+      exec(http("Notifications Read request")
+        .get("/content/notifications?since=${since}")
+        .check(status is 200))
   }
 }
 
-  class ReadSimulation extends Simulation {
+class ReadSimulation extends Simulation {
 
-    val numUsers = Integer.getInteger("users", DefaultNumUsers)
-    val rampUp = Integer.getInteger("ramp-up-minutes", DefaultRampUpDurationInMinutes)
+  val numUsers = Integer.getInteger("users", DefaultNumUsers)
+  val rampUp = Integer.getInteger("ramp-up-minutes", DefaultRampUpDurationInMinutes)
 
-    setUp(
-      ReadSimulation.Scenario.inject(rampUsers(numUsers) over (rampUp minutes))
-    ).protocols(ReadSimulation.HttpConf)
+  setUp(
+    ReadSimulation.Scenario.inject(rampUsers(numUsers) over (rampUp minutes))
+  ).protocols(ReadSimulation.HttpConf)
 
-  }
+}
 
-  class FullSimulation extends Simulation {
+class FullSimulation extends Simulation {
 
-    val numReadUsers = Integer.getInteger("users", DefaultNumUsers)
-    val rampUp = Integer.getInteger("ramp-up-minutes", DefaultRampUpDurationInMinutes)
+  val numReadUsers = Integer.getInteger("users", DefaultNumUsers)
+  val rampUp = Integer.getInteger("ramp-up-minutes", DefaultRampUpDurationInMinutes)
 
-    setUp(
-      ReadSimulation.Scenario.inject(rampUsers(numReadUsers) over (rampUp minutes)).protocols(ReadSimulation.HttpConf)
-    )
+  setUp(
+    ReadSimulation.Scenario.inject(rampUsers(numReadUsers) over (rampUp minutes)).protocols(ReadSimulation.HttpConf)
+  )
 }
