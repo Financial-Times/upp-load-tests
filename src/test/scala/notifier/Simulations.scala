a3d07b3e862d51ab18252becf15ac2b9ceab656c
Carlos Roman <carlos.roman@ft.com>
2016-10-13 10:01:20 +0100

    Updated WriteSimulation to use new global defaults.

--- src/test/scala/notifier/Simulations.scala
+++ src/test/scala/notifier/Simulations.scala
@@ -1,13 +1,9 @@
 package notifier
 
-import java.nio.charset.Charset
-import java.util.UUID
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import utils.LoadTestDefaults._
 
-import scala.concurrent.forkjoin.ThreadLocalRandom.{current => Rnd}
 import scala.language.postfixOps
 
 /**
@@ -15,47 +11,31 @@ import scala.language.postfixOps
   * -Dsoak-duration-minutes=1 -Dhosts="https://semantic-up.ft.com" -Dusername="mashery" -Dpassword="mashery_pass"
   * -Dplatform=coco
   */
-object NotifierSimulation {
+object WriteSimulation {
 
-  val Feeder = csv(System.getProperty("uuid-csv-path", "notifier/one_article.uuid")).random
-
-  val Duration = Integer.getInteger("soak-duration-minutes", DefaultSoakDurationInMinutes)
-
+  val Feeder = getDefaultFeeder("notifier/one_article.uuid")
   val ServiceHeader = "Notifier/Load-test"
   val ServiceEndpoint = "/notify"
   val RequestUrl = ServiceEndpoint
   val ServiceScenario = System.getProperty("endpoint") + " Publish Request"
-  val transactionId = "gatling_" + "${uuid}"
-  val HttpConf = http
-    .baseURLs(System.getProperty("hosts").split(',').to[List])
-    .basicAuth(System.getProperty("username", "username"), System.getProperty("password", "password"))
-    .userAgentHeader(ServiceHeader)
+  val HttpConf = getDefaultHttpConf(ServiceHeader)
 
-  val sentHeaders = Map(
-    "Content-Type" -> "application/json",
-    "X-Origin-System-Id" -> "methode-web-pub",
-    "X-Request-Id" -> transactionId)
-
-    val Scenario = scenario("Notifier Publish").during(Duration minutes) {
-    val b = ELFileBody("notifier/raw_methode_content.json")
-
+  val Scenario = scenario("Notifier Publish").during(Duration minutes) {
     feed(Feeder)
       .exec(
         http("Notifier Write request")
           .post("/notify")
-          .headers(sentHeaders)
-          .body(ELFileBody("notifier/raw_methode_content.json"))
-          .asJSON.check(status is 200))
+          .header("X-Origin-System-Id", "methode-web-pub")
+          .header(RequestIdHeader, (s: Session) => getRequestId(s, "ibw"))
+          .body(ELFileBody("notifier/raw_methode_content.json")).asJSON
+          .check(status is 200))
   }
 }
 
 class WriteSimulation extends Simulation {
 
-  val numUsers = Integer.getInteger("users", DefaultNumUsers)
-  val rampUp = Integer.getInteger("ramp-up-minutes", DefaultRampUpDurationInMinutes)
-
   setUp(
-    NotifierSimulation.Scenario.inject(rampUsers(numUsers) over (rampUp minutes)).protocols(NotifierSimulation.HttpConf)
+    WriteSimulation.Scenario.inject(rampUsers(NumUsers) over (RampUp minutes)).protocols(WriteSimulation.HttpConf)
 
   )
 }
