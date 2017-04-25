df6c32ac8dfeb4d41eb56614ff299383d676487b
Bogdan Guranda <bogdan.guranda@ft.com>
2017-04-25 16:57:55 +0300

    Updated documentation, and moved class and object at the bottom.

--- src/test/scala/enrichedcontent/Simulations.scala
+++ src/test/scala/enrichedcontent/Simulations.scala
@@ -31,8 +31,38 @@ object ReadSimulation {
   }
 }
 
-object StressLimitSimulation {
+class ReadSimulation extends Simulation {
 
+  setUp(
+    ReadSimulation.Scenario.inject(rampUsers(NumUsers) over (RampUp minutes))
+  ).protocols(ReadSimulation.HttpConf)
+
+}
+
+class FullSimulation extends Simulation {
+
+  setUp(
+    ReadSimulation.Scenario.inject(rampUsers(NumUsers) over (RampUp minutes)).protocols(ReadSimulation.HttpConf)
+  )
+
+}
+
+/*
+// StressLimitSimulation is designed as a tool to find out the limit of requests/second that one
+// endpoint is capable of supporting (it stops the requests when it starts to receive error statuses).
+// The reason for stopping is that the purpose of the simulation is reached (i.e. finding the req/sec capability of the endpoint),
+// so there is no need to crush the cluster, which means this simulation is also safe.
+//
+// This simulation considers OK the following HTTP response status codes: 200, 404.
+// Any other status code is consider KO, and the simulation stops sending out request when detected.
+// Note: the simulation is tolerant to accidental error responses, and stops only when
+// the error responses keep coming constantly in a short time.
+//
+// Example of running it from the command line:
+// mvn clean gatling:execute -Dgatling.simulationClass=enrichedcontent.StressLimitSimulation -Dduration-seconds=60 -Dstarting-users-per-sec=100 -Dpeek-users-per-sec=1000
+// -Dhosts="https://pre-prod-up.ft.com" -Dusername="pre-prod" -Dpassword="pre-prod-password" -Dplatform=coco
+*/
+object StressLimitSimulation {
   val Feeder = csv("enrichedcontent/content.uuid." + System.getProperty("platform", "platform")).random
 
   val Duration = Integer.getInteger("duration-seconds", DefaultSoakDurationInSeconds)
@@ -61,27 +91,6 @@ object StressLimitSimulation {
     })
 }
 
-class ReadSimulation extends Simulation {
-
-  setUp(
-    ReadSimulation.Scenario.inject(rampUsers(NumUsers) over (RampUp minutes))
-  ).protocols(ReadSimulation.HttpConf)
-
-}
-
-class FullSimulation extends Simulation {
-
-  setUp(
-    ReadSimulation.Scenario.inject(rampUsers(NumUsers) over (RampUp minutes)).protocols(ReadSimulation.HttpConf)
-  )
-
-}
-
-/*
-// Stress limit test running example:
-// mvn clean gatling:execute -Dgatling.simulationClass=enrichedcontent.StressLimitSimulation -Dduration-seconds=60 -Dstarting-users-per-sec=100 -Dpeek-users-per-sec=1000
-// -Dhosts="https://pre-prod-up.ft.com" -Dusername="pre-prod" -Dpassword="pre-prod-password" -Dplatform=coco
-*/
 class StressLimitSimulation extends Simulation {
   setUp(
     StressLimitSimulation.Scenario.inject(
